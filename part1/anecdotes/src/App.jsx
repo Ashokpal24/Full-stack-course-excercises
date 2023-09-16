298a5ef593a60fed0a7c2c2bcfef0cac6a2a5821
Ashokpal24 <88579810+Ashokpal24@users.noreply.github.com>
2023-09-16 15:19:16 +0000

    Excercise 1.13 completed.

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -11,12 +11,26 @@ const App = () => {
     "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
     "The only way to go fast, is to go well.",
   ];
-
+  const [points, setPoints] = useState(new Array(anecdotes.length).fill(0));
   const [selected, setSelected] = useState(0);
 
+  console.log(points);
   return (
     <>
       <div>{anecdotes[selected]}</div>
+      <br />
+      <div>has {points[selected]} votes</div>
+      <br />
+      <button
+        onClick={() => {
+          const newPoints = [...points];
+          newPoints[selected] += 1;
+          setPoints(newPoints);
+        }}
+      >
+        Vote
+      </button>
+
       <button
         onClick={() =>
           setSelected(Math.floor(Math.random() * anecdotes.length))
