9ec4c0397cce60ad016cba74e90c0722c7b6f68a
Ashokpal24 <88579810+Ashokpal24@users.noreply.github.com>
2023-09-16 09:06:06 +0000

    Excercise 1.6 completed

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+const App = () => {
+  const [good, SetGood] = useState(0);
+  const [neutral, SetNeutral] = useState(0);
+  const [bad, SetBad] = useState(0);
+
+  return <div>Code here</div>;
+};
+
+export default App;
