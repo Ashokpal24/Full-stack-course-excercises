38b2f1fa74831be35d26b2a6f3cafe21fdae82ab
Ashokpal24 <88579810+Ashokpal24@users.noreply.github.com>
2023-09-16 09:32:41 +0000

    Excercise 1.7 completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,8 +4,47 @@ const App = () => {
   const [good, SetGood] = useState(0);
   const [neutral, SetNeutral] = useState(0);
   const [bad, SetBad] = useState(0);
+  const [total, setTotal] = useState(0);
 
-  return <div>Code here</div>;
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <button
+        onClick={() => {
+          const newValue = good + 1;
+          SetGood(newValue);
+          setTotal(newValue + neutral + bad);
+        }}
+      >
+        Good
+      </button>
+      <button
+        onClick={() => {
+          const newValue = neutral + 1;
+          SetNeutral(newValue);
+          setTotal(good + newValue + bad);
+        }}
+      >
+        Neutral
+      </button>
+      <button
+        onClick={() => {
+          const newValue = bad + 1;
+          SetBad(newValue);
+          setTotal(good + neutral + newValue);
+        }}
+      >
+        Bad
+      </button>
+      <h1>Statics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral:{neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>Total: {total}</p>
+      <p>Average: {(good * 1 + neutral * 0 + bad * -1) / total}</p>
+      <p>Positive: {(good / total) * 100}%</p>
+    </div>
+  );
 };
 
 export default App;
