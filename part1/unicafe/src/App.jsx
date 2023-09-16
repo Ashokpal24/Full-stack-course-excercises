9c874f76630db8e10fe714564e7cf08938afe6e8
Ashokpal24 <88579810+Ashokpal24@users.noreply.github.com>
2023-09-16 14:21:24 +0000

    Excercise 1.10 completed.

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,55 +4,74 @@ const Statistics = ({ good, bad, neutral, total }) => {
   if (total) {
     return (
       <>
-        <p>Good: {good}</p>
-        <p>Neutral:{neutral}</p>
-        <p>Bad: {bad}</p>
-        <p>Total: {total}</p>
-        <p>Average: {(good * 1 + neutral * 0 + bad * -1) / total}</p>
-        <p>Positive Percentage: {(good / total) * 100}%</p>
+        <StatisticsLine text={"Good"} value={good} />
+        <StatisticsLine text={"Neutral"} value={neutral} />
+        <StatisticsLine text={"Bad"} value={bad} />
+        <StatisticsLine text={"Total"} value={total} />
+        <StatisticsLine
+          text={"Average"}
+          value={(good * 1 + neutral * 0 + bad * -1) / total}
+        />
+        <StatisticsLine
+          text={"Positive Percentage"}
+          value={`${(good / total) * 100} %`}
+        />
       </>
     );
   }
-  return <p1>No feedback given</p1>;
+  return <p>No feedback given</p>;
 };
 
+const StatisticsLine = ({ text, value }) => {
+  return (
+    <p>
+      {text}: {value}
+    </p>
+  );
+};
+
+const Button = ({ text, setValue, newValue }) => {
+  return (
+    <button
+      onClick={() => {
+        setValue(newValue);
+      }}
+    >
+      {text}
+    </button>
+  );
+};
 const App = () => {
   const [good, SetGood] = useState(0);
   const [neutral, SetNeutral] = useState(0);
   const [bad, SetBad] = useState(0);
   const [total, setTotal] = useState(0);
 
+  const updateGood = (newValue) => {
+    SetGood(newValue);
+    setTotal(newValue + neutral + bad);
+  };
+
+  const updateNeutral = (newValue) => {
+    SetNeutral(newValue);
+    setTotal(good + newValue + bad);
+  };
+
+  const updateBad = (newValue) => {
+    SetBad(newValue);
+    setTotal(good + neutral + newValue);
+  };
+
   return (
     <div>
       <h1>Give Feedback</h1>
-      <button
-        onClick={() => {
-          const newValue = good + 1;
-          SetGood(newValue);
-          setTotal(newValue + neutral + bad);
-        }}
-      >
-        Good
-      </button>
-      <button
-        onClick={() => {
-          const newValue = neutral + 1;
-          SetNeutral(newValue);
-          setTotal(good + newValue + bad);
-        }}
-      >
-        Neutral
-      </button>
-      <button
-        onClick={() => {
-          const newValue = bad + 1;
-          SetBad(newValue);
-          setTotal(good + neutral + newValue);
-        }}
-      >
-        Bad
-      </button>
-
+      <Button text={"Good"} setValue={updateGood} newValue={good + 1} />
+      <Button
+        text={"Neutral"}
+        setValue={updateNeutral}
+        newValue={neutral + 1}
+      />
+      <Button text={"Bad"} setValue={updateBad} newValue={bad + 1} />
       <h1>Statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad} total={total} />
     </div>
